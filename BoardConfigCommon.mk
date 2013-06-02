41af3b4cde1e0128b092b702d1d90f76ef75062c
Dorian Snyder <dastin1015@gmail.com>
2013-06-02 02:02:49 -0700

    shooter-common: initial repo for common shooter repo

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,62 @@
+#
+# Copyright (C) 2011 The Evervolv Project
+# Portions Copyright (C) 2011 The CyanogenMod Project
+# Copyright (C) 2013 Dorian Snyder
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+
+# inherit from common msm8660
+-include device/htc/msm8660-common/BoardConfigCommon.mk
+
+# Broadcom specific config
+-include device/htc/msm8660-common/bcm.mk
+
+BOARD_KERNEL_BASE := 0x48000000
+BOARD_KERNEL_PAGE_SIZE := 2048
+
+COMMON_GLOBAL_CFLAGS += -DQCOM_ROTATOR_KERNEL_FORMATS
+TARGET_USES_OVERLAY := true
+
+# Hardware Rendering
+TARGET_USES_PMEM := true
+
+# Camera
+BOARD_HTC_3D_SUPPORT := true
+
+# Kernel
+TARGET_KERNEL_SOURCE   := kernel/htc/msm8660
+BUILD_KERNEL           := true
+
+# Custom lun file path
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun0/file
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_FLASH_BLOCK_SIZE := 262144
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
+BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
+BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_NO_SELECT_BUTTON := true
---
+++ shooter-common.mk
@@ -0,0 +1,64 @@
+#
+# Copyright (C) 2011 The Evervolv Project
+# Portions Copyright (C) 2011 The CyanogenMod Project
+# Copyright (C) 2013 Dorian Snyder
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+COMMON_PATH := device/htc/shooter-common
+
+## overlays
+DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay
+
+# common msm8660 configs
+$(call inherit-product, device/htc/msm8660-common/msm8660.mk)
+
+# Vold config
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/config/vold.fstab:system/etc/vold.fstab
+
+# idc
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/config/atmel-touchscreen.idc:system/usr/idc/atmel-touchscreen.idc
+
+# BCM4329 BT Firmware
+PRODUCT_COPY_FILES += \
+    device/htc/msm8660-common/firmware/bcm4329.hcd:system/vendor/firmware/bcm4329.hcd
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+
+# GPS
+PRODUCT_COPY_FILES += device/common/gps/gps.conf_US_SUPL:system/etc/gps.conf
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# misc
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.setupwizard.enable_bypass=1 \
+    dalvik.vm.lockprof.threshold=500 \
+    ro.com.google.locationfeatures=1 \
+    dalvik.vm.dexopt-flags=m=y \
+    ro.vold.umsdirtyratio=20 \
+    htc.audio.alt.enable=1 \
+    htc.audio.hac.enable=0
+
+# Inherite make files
+$(call inherit-product, build/target/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329/device-bcm.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
